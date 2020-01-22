64a36f13c1506f699cc4400d440889006de58bff
Andrea Girardi <andrea.girardi@gmail.com>
2020-01-22 17:07:26 +0100

    Create ProgrammingBasic.r

---
+++ ProgrammingBasic.r
@@ -0,0 +1,15 @@
+# 
+# Don't complain; just work harder! Dan Stanford
+# 
+# Our responsibility is to do what we can, learn what we can, improve the solutions, and pass them on. RF
+# 
+# @author: me@andreagirardi.it
+# @since: Wed Jan 15 18:34:22 CET 2020
+# @project: RExercises
+# @module: Programming basic lesson
+# @desc: 
+#
+#
+# Exercises
+
+ 
