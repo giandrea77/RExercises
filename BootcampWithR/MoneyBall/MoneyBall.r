07e4ad075a19d6ddf6093869c2e7e7b1683864b0
Andrea <andrea.girardi@gmail.com>
2020-04-21 18:19:21 +0200

    Update

---
+++ BootcampWithR/MoneyBall/MoneyBall.r
@@ -0,0 +1,18 @@
+# 
+# Don't complain; just work harder! Dan Stanford
+# 
+# Our responsibility is to do what we can, learn what we can, improve the solutions, and pass them on. RF
+# 
+# @author: me@andreagirardi.it
+# @since: Wed Jan 22 17:29:30 CET 2020
+# @project: RExercises
+# @module: ggplot2 samples
+# @desc: 
+#
+# @prerequisite
+#   install.packages('ggplot2')
+#   library(ggplot2)
+#   library(ggthemes)
+#   mtcars
+
+> library(ggplot2)
